6f627ba3a652285a87623612850694aaf1d29e14
bunrealcua <voduynhan.123@gmail.com>
2022-11-22 15:02:17 +0700

    Search keyword

--- src/Main.java
+++ src/Main.java
@@ -12,39 +12,50 @@ import java.util.Scanner;
 // 2. Tao menu console tam thoi
 // 3. Lam cac chuc nang
 public class Main {
-
-    public static void  main(String[] args) throws IOException {
-        // Read data from slang.txt file
+    public static HashMap<String, HashMap<String, Boolean>> LoadData () throws IOException
+    {
+// Read data from slang.txt file
         HashMap<String, HashMap<String, Boolean>> keywordSlangWordDictionary = new HashMap<String, HashMap<String, Boolean>>();
-        BufferedReader br = new BufferedReader(new FileReader("./src/slang.txt"));
+        HashMap<String, Boolean> slangWord;
+        BufferedReader br = new BufferedReader(new FileReader("slang.txt"));
         String line = br.readLine(); // SKIP 1ST LINE: SLANG`MEANING
         line = br.readLine();
 
         while (line != null)
         {
             String[] parts = line.split("`");
-            System.out.println(parts);
-            if (parts.length != 2) continue;
-            else {
+            if (parts.length == 2)
+            {
                 String keyword = "";
                 for (int i = 0 ; i < parts[0].length(); i ++) {
                     keyword = keyword + parts[0].charAt(i);
                     if (keywordSlangWordDictionary.get(keyword) == null) {
-                        HashMap<String, Boolean> slangWord = new HashMap<String, Boolean>();
+                        slangWord = new HashMap<String, Boolean>();
                         slangWord.put(parts[0], true);
                         keywordSlangWordDictionary.put(keyword,slangWord);
                     }
                     else {
-                        HashMap<String, Boolean> slangWord = keywordSlangWordDictionary.get(keyword);
+                        slangWord = keywordSlangWordDictionary.get(keyword);
                         slangWord.put(parts[0], true);
                     }
                 }
 
             }
             line = br.readLine();
+
         }
 
+        return keywordSlangWordDictionary;
+    }
+    public static void  main(String[] args) throws IOException {
 
+    HashMap<String , HashMap<String, Boolean>> keywordSlangWordDictionary = LoadData();
 
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter keyword: ");
+    String keyword = scanner.nextLine();
+
+    HashMap <String, Boolean> slang = keywordSlangWordDictionary.get(keyword);
+    System.out.println(slang);
     }
 }
