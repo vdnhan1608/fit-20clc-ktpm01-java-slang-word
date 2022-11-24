ec8d271c25217fe64e9602302b08d082b28bee66
bunrealcua <voduynhan.123@gmail.com>
2022-11-24 10:50:06 +0700

    build search by keyword screen: miss scroll bar, and not complete

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,16 +32,68 @@ public class Main  {
     public static JButton guessDefinitionBtn = new JButton("Guess definition of slang word");
     public static JButton guessSlangWordBtn = new JButton("Guess slang words");
 
+    public static SlangWord SlangWord = new SlangWord();
     /*
-    * Set up listener for the button
+    * These JFrame are all screen of application
     * */
-    public static void setUpListener(){}
+    public static JFrame mainScreen = new JFrame("Slang word");
 
-    public static void createAndShowUI()
+    public static JFrame searchByKeywordScreen = new JFrame("Search by keyword");
+
+    /*
+    * Desc: Create search by keyword screen
+    * */
+    public static void searchByKeywordScreen()
     {
-        JFrame frame = new JFrame("Slang Word");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(500, 600));
+        searchByKeywordScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        searchByKeywordScreen.setSize(200, 300);
+
+        Container contentPane = searchByKeywordScreen.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        JPanel pane1 = new JPanel();
+        pane1.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("Search");
+        pane1.add(label);
+
+        JTextField textField = new JTextField(10);
+        pane1.add(textField);
+
+        JPanel pane2 = new JPanel();
+        JTextArea textArea = new JTextArea(50,50);
+//        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+//        pane2.add(scrollPane);
+        // scrollPane.setViewport(textArea);
+        pane2.add(textArea);
+
+        contentPane.add(pane1,BorderLayout.PAGE_START);
+        contentPane.add(pane2, BorderLayout.PAGE_END);
+
+        /*
+        * Xu ly su kien
+        * */
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String keyword = textField.getText();
+
+                String[] slangWords = SlangWord.searchSlangWordByKeyWord(keyword).keySet().toArray(new String[0]);
+                for (int i = 0; i < slangWords.length; i ++)
+                textArea.append(slangWords[i]+'\n');
+            }
+        });
+
+        searchByKeywordScreen.pack();
+    }
+
+    /*
+    * Desc: Create main screen
+    * */
+    public static void mainScreen()
+    {
+        mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainScreen.setPreferredSize(new Dimension(500, 600));
 
         JPanel pane = new JPanel();
         pane.setLayout(new GridLayout(5,2));
@@ -62,20 +116,40 @@ public class Main  {
         pane.add(guessDefinitionBtn);
 
         JLabel label = new JLabel("SLANG WORD APPLICATION");
-        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-        frame.getContentPane().add(label);
-        frame.getContentPane().add(pane);
+        mainScreen.getContentPane().setLayout(new BoxLayout(mainScreen.getContentPane(), BoxLayout.Y_AXIS));
+        mainScreen.getContentPane().add(label);
+        mainScreen.getContentPane().add(pane);
 
-        frame.pack();
-        frame.setVisible(true);
+        mainScreen.pack();
+//        frame.setVisible(true);
     }
+    /*
+    * Set up listener for the button
+    * */
+    public static void setUpListener()
+    {
+        searchBtnByDefinition.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("search by definition");
+            }
+        });
+    }
+
+
     public static void  main(String[] args) throws IOException {
-        SlangWord SlangWord = new SlangWord();
+
         SlangWord.loadDataByDefinition();
         SlangWord.loadDataByKeyWord();
         SlangWord.loadData();
 
-        createAndShowUI();
+        //mainScreen();
+        searchByKeywordScreen();
+        setUpListener();
+
+        searchByKeywordScreen.setVisible(true);
+
+
 
 
 
